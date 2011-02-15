82bccb684ff09a4ce943596f75918f30c92fa13d
Thomas Watson Steen <w@tson.dk>
2011-02-15 18:38:25 +0100

    When changeing the timezone on the server, crontab needs to be restarted to use the new zone

--- cookbooks/timezone/recipes/default.rb
+++ cookbooks/timezone/recipes/default.rb
@@ -5,4 +5,6 @@
 
 execute "Set server timezone to Copenhagen" do
   command "ln -sf /usr/share/zoneinfo/Europe/Copenhagen /etc/localtime"
+  command "sudo /etc/init.d/vixie-cron stop"
+  command "sudo /etc/init.d/vixie-cron start"
 end
