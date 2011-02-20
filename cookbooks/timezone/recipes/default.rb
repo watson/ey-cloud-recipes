9c8ccb5a54d5fba12b0e082e17490e3d9894191e
Thomas Watson Steen <w@tson.dk>
2011-02-20 23:22:17 +0100

    Updated our recipes

--- cookbooks/timezone/recipes/default.rb
+++ cookbooks/timezone/recipes/default.rb
@@ -5,6 +5,12 @@
 
 execute "Set server timezone to Copenhagen" do
   command "ln -sf /usr/share/zoneinfo/Europe/Copenhagen /etc/localtime"
+end
+
+execute "Stop crontab" do
   command "sudo /etc/init.d/vixie-cron stop"
+end
+
+execute "Start crontab" do
   command "sudo /etc/init.d/vixie-cron start"
 end
